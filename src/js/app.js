b5fa379b7768f44d1ee10791548f97602f009981
b3m3 <b3m3@yandex.ru>
2022-05-08 15:23:55 +0300

    create burger.js, moveElement.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,11 @@
 import webp from './lib/webp.js';
+import burger from './lib/burger.js';
+import moveElement from './lib/moveElement.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   'use strict';
 
   webp();
+  burger();
+  moveElement(601, '.wrapp-btn', '.btn-elem');
 });
